644acebb85ae7d5a2bffb3fff1671b86bbfdda14
Michael Andrea <man_ext@michaels-mbp-3.home>
2018-05-25 13:41:44 +0100

    Initial commit

---
+++ app/src/main/java/com/example/man_ext/boundary/Homescreen.java
@@ -0,0 +1,13 @@
+package com.example.man_ext.boundary;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class Homescreen extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_homescreen);
+    }
+}
